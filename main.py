50a82fb1f61858a909dac51ec160ca13014ded87
Alexandra Levy <alevy24@biomedscienceacademy.org>
2021-01-22 16:54:02 +0000

    I fixed it so that the capitalization doesn't matter and so that the program does not just use the first if statement. I did this by using .lower() and by putting the equal sign in the ors throughout the if statements.

--- main.py
+++ main.py
@@ -30,43 +30,44 @@ def week():
   global day1
   day =input("What is your favorite day of the week?\n")
   day1 = (day)
-  if day =="Monday":
+  day=day.lower()
+  if day =="monday":
     mon = input("Ew. Why Monday?\n")
     if mon=="IDK":
       print("Ok nevermind then.")
     else:
       print("Strange.")
-  elif day=="Tuesday":
+  elif day=="tuesday":
     tues = input("Oooo Taco Tuesday. Why is it your favorite?\n")
     if tues=="IDK":
       print("Ok Nevermind then.")
     else:
       print("That's a cool take on Tuesdays!")
-  elif day=="Wednesday":
+  elif day=="wednesday":
     wed = input("Why Wednesday my dude?\n")
     if wed=="IDK":
       print("Ok nevermind then.")
     else:
       print("Wenesdays always seem to pass by so quickly.")
-  elif day=="Thursday":
+  elif day=="thursday":
     thurs = input("Friday Jr! Why is that one your favorite?\n")
     if thurs=="IDK":
       print("Ok Nevermind then.")
     else:
       print("Well then I hope you enjoy your next Friday Jr.")
-  elif day =="Friday":
+  elif day =="friday":
     fri = input("Someone with good taste. Why is it your favorite day of the week?\n")
     if fri=="IDK":
       print("Ok nevermind then.")
     else:
       print("May I reccommend: Katy Perry's hit song \"Last Friday night?\"")
-  elif day=="Saturday":
+  elif day=="saturday":
     sat = input("Ahhh Saturday the day of rest. Why is it your favorite?\n")
     if sat =="IDK":
       print("Ok nevermind then.")
     else:
       print("I hope that you enjoy your next Saturday!")
-  elif day=="Sunday":
+  elif day=="sunday":
     sun = input("The sunniest of the days. Why is Sunday your favorite?\n")
     if sun =="IDK":
       print("Ok nevermind then.")
@@ -76,9 +77,10 @@ def week():
     print("I wasn't expecting that.")
   print("I feel like you can learn a lot about a person based on their favorite day of the week.")
   quest = input("Do you agree?\n")
-  if quest=="Yes":
+  quest = quest.lower()
+  if quest=="yes":
     print("It's nice to know thaht we share the same opinion!")
-  elif quest =="No":
+  elif quest =="no":
     print("It's ok for people to have varrying opinions.")
   else:
     print("I wasn't expecting that.")
@@ -86,20 +88,21 @@ def holidays():
   global holiday
   print("Enough about days of the week. Let's move on to more special days.")
   holiday = input("What is your favorite holiday?\n")
-  if holiday == "Christmas":
+  holiday = holiday.lower()
+  if holiday == "christmas":
     chris = input("A very jolly choice indeed. Why is Christmas your favorite holiday?\n")
     if chris =="IDK":
       print("Ok nevermind then.")
     else:
       print("I hope that you feel the Christmas spirit all the time!")
-  elif holiday == "Easter":
+  elif holiday == "easter":
     east = input("I've never met anyone who's favorite holiday is Easter. Why is it your favorite?\n")
     if east =="IDK":
       print("Ok nevermind then.")
     else:
       print("I hope that you have fun on your next bunny day!")
-  elif holiday == "New Year's":
-    new = input("New Years! Why is that your favorite?\n")
+  elif holiday == "new year's":
+    new = input("New Year's! Why is that your favorite?\n")
     if new =="IDK":
       print("Ok nevermind then.")
     else:
@@ -108,61 +111,66 @@ def holidays():
     extra = input("I wasn't expecting that. Why is it your favorite?\n")
     print("Such an interesting perspective!")
 def intro():
-  emote = input("How are you feeling today?\n")
-  if emote == "Sad":
-    x = random.randint(0,1)
-    if x==0:
-      print("I'm so sorry to hear that. Just hang on and I'm sure today will get better!")
-    elif x==1:
-      water=input("Did you dirnk water today?\n")
-      if water == "Yes":
-        print("Good job! I'm proud of you just keep making progress!")
-      elif water=="No":
-        print("Try to drink a cup. It might not seem like it will help but I promise that it will!")
-      else:
-        print("I wasn't expecting that.")
-  elif emote=="Happy":
-    y = random.randint(0,1)
-    if y==0:
-      print("That's great to hear!")
-    elif y==1:
-      print("Keep up the positive attidude!")
-  else:
-     input("Well I wasn't expecting that. Why do you feel that way?\n")
-     print("That's very interesting.")
+ emote = input("How are you feeling today?\n")
+ emote = emote.lower()
+ if emote=="good" or emote=="happy":
+   y=random.randint(0,1)
+   if y ==1:
+     print("Well that is great to hear!")
+   else:
+     print("I'm so happy for you!")
+ elif emote=="sad" or emote=="sad":
+   x=random.randint(0,1)
+   if x==1:
+     print("Aww I'm so sorry just remember tomorrow is another day.")
+   else:
+     water=input("Did you drink water today?\n")
+     water=water.lower()
+     if water =="yes":
+       print("Good job! That's progress!")
+     elif water=="no":
+       print("I know it seems like a small step but try to drink a cup it can really help!")
+     else:
+       print("I wasn't expecting that.")
+ else:
+    print("I wasn't expecting that.")
+    input("Why do you feel this way?\n")
+    print("Interesting.")   
 def zodiac():
   global zod
-  zod=input("What is your Zodiac sign?")
-  if zod == "Cancer" or "Pisces" or "Scorpio":
+  zod=input("What is your Zodiac sign?\n")
+  zod =zod.capitalize()
+  if zod == "Cancer" or zod=="Pisces" or zod=="Scorpio":
     print("You're a water sign!")
-  elif zod == "Gemini" or "Libra" or "Aquarius":
+  elif zod == "Gemini" or zod=="Libra" or zod=="Aquarius":
     print("You're an air sign!")
-  elif zod == "Taurus" or "Virgo" or "Capricorn":
+  elif zod == "Taurus" or zod=="Virgo" or zod=="Capricorn":
     print("You're an earth sign!")
-  elif zod == "Aries" or "Leo" or "Aquarius":
+  elif zod == "Aries" or zod=="Leo" or zod=="Aquarius":
     print("You're a fire sign!")
   else:
     print("I wasn't expecting that.")
 def conclusion():
-  print("Well look at what I've learned about you today!\n")
+  global day1
+  global holiday
+  print("Well look at what I've learned about you today "+ name1 + "!")
   print("Your zodiac sign is a/an ", end='')
   print(zod)
   print("Your favorite day of the week is ", end='')
+  day1=day1.capitalize()
   print(day1)
   print("Your favorite holiday is ", end='')
+  holiday=holiday.capitalize()
   print(holiday)
   print("It was great getting to know you!")
-
 def main():
   """This function contains all code for the chatbot."""
-  print("Hello!")
+  print("Hello! ",end='')
   intro()
   zodiac()
   week()
   holidays()
   conclusion()
-
-
 
 
 if __name__ == "__main__":
